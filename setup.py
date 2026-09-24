fba9fc214b32472269deb659bc6a21745f3de2bb
peterpasem827_beep <peterpasem827@gmail.com>
2026-09-24 21:07:46 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup( name = 'Ai-medical',
+      version = '0.1',
+      author = 'peter',
+        packages = find_packages(),
+        author_email = "peterpasem827@gmail.com"
+        install_requires = []
+        )
